3a34a55f409f646bfe053292db47375ced3d6b73
oobi <matt_wendel@hotmail.com>
2020-05-07 17:32:23 -0400

    LOGIN BROKEN

--- Semplicita/Controllers/AccountController.cs
+++ Semplicita/Controllers/AccountController.cs
@@ -58,7 +58,7 @@ namespace Semplicita.Controllers
         public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
-            return View();
+            return View(new LoginViewModel());
         }
 
         //
