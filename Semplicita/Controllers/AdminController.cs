d1136d89a6aeb5452cb7a50a3f4c9e6ba98d913d
oobi <matt_wendel@hotmail.com>
2020-05-12 11:49:34 -0400

    working on admin views;

--- Semplicita/Controllers/AdminController.cs
+++ Semplicita/Controllers/AdminController.cs
@@ -1,8 +1,16 @@
-﻿using Semplicita.Helpers;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Semplicita.Helpers;
 using Semplicita.Models;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace Semplicita.Controllers
@@ -11,6 +19,17 @@ namespace Semplicita.Controllers
     public class AdminController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
+
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager {
+            get {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set {
+                _userManager = value;
+            }
+        }
+
         private UserRolesHelper rolesHelper = new UserRolesHelper();
         private ProjectHelper projectHelper = new ProjectHelper();
 
@@ -19,6 +38,10 @@ namespace Semplicita.Controllers
         public ActionResult Index() {
             return View();
         }
+
+        #region User Managment
+        #endregion
+
 
         #region Project Managment
 
@@ -102,6 +125,96 @@ namespace Semplicita.Controllers
             return RedirectToAction("Index");
         }
 
+
+        [Authorize(Roles = "ServerAdmin,ProjectAdmin")]
+        public ActionResult ProjectUsers() {
+            var model = new ProjectUsersViewModel() { Users = db.Users.ToList(), Projects = db.Projects.ToList() };
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ServerAdmin,ProjectAdmin")]
+        public ActionResult AddUsersToProject([Bind(Include = "UserIds,ProjectIds")]AddUserToProjectViewModel model) {
+            var errors = new List<string>();
+
+            foreach( string userId in model.UserIds ) {
+                var user = db.Users.FirstOrDefault(u => u.Id == userId);
+
+                if( user != null ) {
+
+                    foreach( int projectId in model.ProjectIds ) {
+                        var addRoleSuccess = projectHelper.AddUserToProject(userId, projectId);
+
+                        if( !addRoleSuccess ) {
+                            errors.Add($"Unable to add '{user.FullNameStandard}' to project '{db.Projects.Find(projectId).Name}'.");
+                        }
+                    }
+
+
+                } else { //user ID was not found.
+
+                    if( !errors.Contains("Unable to locate one or more selected users.") ) {
+                        errors.Add("Unable to locate one or more selected users.");
+                    }
+
+                }
+
+            }
+
+            if( errors.Count > 0 ) {
+                ViewBag.AddUsersToProjectErrors = errors;
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "ServerAdmin,ProjectAdmin")]
+        public ActionResult RemoveUserFromProject([Bind(Include = "UserIds,ProjectIds")]RemoveUserFromProjectViewModel model) {
+            var errors = new List<string>();
+
+            foreach( string userId in model.UserIds ) {
+                var user = db.Users.FirstOrDefault(u => u.Id == userId);
+
+                if( user != null ) {
+
+                    foreach( int projectId in model.ProjectIds ) {
+                        var addRoleSuccess = projectHelper.AddUserToProject(userId, projectId);
+
+                        if( !addRoleSuccess ) {
+                            errors.Add($"Unable to add '{user.FullNameStandard}' to project '{db.Projects.Find(projectId).Name}'.");
+                        }
+                    }
+
+
+                } else { //user ID was not found.
+
+                    if( !errors.Contains("Unable to locate one or more selected users.") ) {
+                        errors.Add("Unable to locate one or more selected users.");
+                    }
+
+                }
+
+            }
+
+            if( errors.Count > 0 ) {
+                ViewBag.AddUsersToProjectErrors = errors;
+            }
+
+            return View();
+        }
+
         #endregion Project Managment
+
+
+
+
+        #region Helpers
+        private void AddErrors(IdentityResult result) {
+            foreach( var error in result.Errors ) {
+                ModelState.AddModelError("", error);
+            }
+        }
+        #endregion
     }
 }
---
+++ Semplicita/Models/AdminViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Semplicita.Models
+{
+    public class ProjectUsersViewModel
+    {
+        public ICollection<ApplicationUser> Users { get; set; }
+        public ICollection<Project> Projects { get; set; }
+    }
+
+    public class AddUserToProjectViewModel
+    {
+        public ICollection<string> UserIds { get; set; }
+        public ICollection<int> ProjectIds { get; set; }
+    }
+    public class RemoveUserFromProjectViewModel
+    {
+        public ICollection<string> UserIds { get; set; }
+        public ICollection<int> ProjectIds { get; set; }
+
+    }
+
+}
