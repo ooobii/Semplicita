c3b16be65c8fbb4dc5afc437f9f45301aa99a9a2
Matthew <60083197+ooobii@users.noreply.github.com>
2020-05-12 21:33:46 -0400

    Add TicketHistoryEntryType
    Add TicketHistoryEntryType enum to TicketHistoryEntry model; will be used for case selecting the HTML element required for properly displaying the old data & new data.

--- Semplicita/Models/TicketComponents/TicketHistoryEntry.cs
+++ Semplicita/Models/TicketComponents/TicketHistoryEntry.cs
@@ -2,6 +2,20 @@
 {
     public class TicketHistoryEntry
     {
+        public enum TicketHistoryEntryType
+        {
+            Created = 0,
+            AssignedToSolver = 1,
+            ReAssignedToSolver = 2,
+            StatusChange = 3,
+            AttachmentAdded = 4,
+            CommentAdded = 5,
+            Archived = 6,
+            TicketTypeModified = 9,
+            TitleModified = 7,
+            DescriptionModified = 8
+        }
+
         public int Id { get; set; }
 
         public int ParentTicketId { get; set; }
@@ -9,5 +23,10 @@
 
         public virtual Ticket ParentTicket { get; set; }
         public virtual ApplicationUser User { get; set; }
+
+
+        public TicketHistoryEntryType EntryType { get; set; }
+        public string OldData { get; set; }
+        public string NewData { get; set; }
     }
 }
