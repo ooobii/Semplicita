ad74d3ec37f7c987219791fc30d02ce6d91c0947
Matthew <60083197+ooobii@users.noreply.github.com>
2020-05-20 16:59:37 -0400

    add bootstrap-duallistbox
    downloaded from NPM package and imported to project (no NPM link in project)

---
+++ Semplicita/Models/ProjectViewModels.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Semplicita.Models
+{
+    public class CreateProjectViewModel
+    {
+        public ICollection<ApplicationUser> ProjectAdministrators { get; set; }
+        public ICollection<ApplicationUser> AvailableMembers { get; set; }
+        public ICollection<ProjectWorkflow> Workflows { get; set; }
+    }
+    public class NewProjectModel
+    {
+        [Required]
+        public string Name { get; set; }
+        
+
+        [Required]
+        public string Description { get; set; }
+
+
+        [Required]
+        [StringLength(3, MinimumLength = 1)]
+        public string TicketTag { get; set; }
+
+
+        [Required]
+        public int ActiveWorkflowId { get; set; }
+
+
+        [Required]
+        public string ProjectManagerId { get; set; }
+
+
+        [Required]
+        public bool IsActiveProject { get; set; }
+        
+
+        [Required]
+        public ICollection<string> MemberIds { get; set; }
+       
+    }
+}
