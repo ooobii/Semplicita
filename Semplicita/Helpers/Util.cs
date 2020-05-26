b03e1fb3584061217a97fec72898d0c016b1bf92
Matthew <60083197+ooobii@users.noreply.github.com>
2020-05-26 10:46:18 -0400

    Create Util class
    contains method as shorthand to read from application configuration

---
+++ Semplicita/Helpers/Util.cs
@@ -0,0 +1,21 @@
+﻿public static class Util
+{        
+    public static T GetSetting<T>(string name) {
+        string value = System.Configuration.ConfigurationManager.AppSettings[ name ];
+
+        if( value == null ) {
+            throw new System.Exception(string.Format("Could not find setting '{0}',", name));
+        }
+
+        return (T)System.Convert.ChangeType(value, typeof(T), System.Globalization.CultureInfo.InvariantCulture);
+    }
+    public static string GetSetting(string name) {
+        string value = System.Configuration.ConfigurationManager.AppSettings[ name ];
+
+        if( value == null ) {
+            throw new System.Exception(string.Format("Could not find setting '{0}',", name));
+        }
+
+        return (string)System.Convert.ChangeType(value, typeof(string), System.Globalization.CultureInfo.InvariantCulture);
+    }
+}
