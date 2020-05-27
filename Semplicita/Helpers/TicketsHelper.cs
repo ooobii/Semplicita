c2ed7ca57e0b5b09f3dcc82219899f316f5652a8
Matthew <60083197+ooobii@users.noreply.github.com>
2020-05-27 10:19:20 -0400

    Create TicketsHelper

---
+++ Semplicita/Helpers/TicketsHelper.cs
@@ -0,0 +1,39 @@
+﻿using Semplicita.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+
+namespace Semplicita.Helpers
+{
+    public class TicketsHelper
+    {
+        public ApplicationDbContext db { get; set; }
+        public TicketsHelper(ApplicationDbContext context) {
+            db = context;
+        }
+
+        public List<Ticket> GetTicketsAvailableToUser(IPrincipal User) {
+            List<Ticket> tickets = new List<Ticket>();
+
+            if( User.IsInRole("ServerAdmin") ) {
+                tickets = db.Tickets.ToList();
+
+            } else if( User.IsInRole("ProjectAdmin") ) {
+                tickets = db.Tickets.Where(t => t.ParentProject.ProjectManagerId == User.Identity.GetUserId()).ToList();
+
+            } else if( User.IsInRole("SuperSolver") ) {
+                tickets = db.Tickets.Where(t => t.ParentProject.Members.Contains(db.Users.Find(User.Identity.GetUserId()))).ToList();
+
+            } else if( User.IsInRole("Solver") ) {
+                tickets = db.Tickets.Where(t => t.AssignedSolver == db.Users.Find(User.Identity.GetUserId())).ToList();
+
+            } else if( User.IsInRole("Reporter") ) {
+                tickets = db.Tickets.Where(t => t.Reporter == db.Users.Find(User.Identity.GetUserId())).ToList();
+            }
+
+            return tickets;
+        }
+
+    }
+}
