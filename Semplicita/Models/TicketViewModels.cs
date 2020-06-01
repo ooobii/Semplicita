3dec56c349ed80280b880228a40c70b5737a6b1d
Matthew <60083197+ooobii@users.noreply.github.com>
2020-06-01 04:02:15 -0400

    Add TicketViewModels
    Added TicketViewModels file to hold classes for manipulating Tickets.

---
+++ Semplicita/Models/TicketViewModels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Semplicita.Models
+{
+    public class CreateTicketViewModel
+    {
+        public Project ParentProject { get; set; }
+        public ApplicationUser Reporter { get; set; }
+
+        public Dictionary<string, int> PrioritySelections { get; set; }
+        public Dictionary<string, int> AvailableStatuses { get; set; }
+        public ICollection<TicketType> AvailableTicketTypes { get; set; }
+
+    }
+    public class NewTicketModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        
+        public int ParentProjectId { get; set; }
+        public int TicketTypeId { get; set; }
+        public int TicketPriorityId { get; set; }
+        public int? TicketStatusId { get; set; }
+
+
+        public ICollection<HttpPostedFileBase> InitialAttachments { get; set; }
+    }
+}
