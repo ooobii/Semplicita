b6e705b194e309444c7c8bdd00026dd9f17506d3
Matthew <60083197+ooobii@users.noreply.github.com>
2020-06-01 04:11:29 -0400

    Add helper methods for ticket attachment creation
    Added method to convert posted HTTP file as a saved file on the disk, and convert it into a TicketAttachment model object.

--- Semplicita/Models/TicketComponents/TicketAttachment.cs
+++ Semplicita/Models/TicketComponents/TicketAttachment.cs
@@ -1,4 +1,10 @@
-﻿namespace Semplicita.Models
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.IO;
+using System.Security.Principal;
+using System.Web;
+
+namespace Semplicita.Models
 {
     public class TicketAttachment
     {
@@ -8,8 +14,38 @@
         public string AuthorId { get; set; }
 
         public string Name { get; set; }
+        public string MediaUrl { get; set; }
+
 
         public virtual Ticket ParentTicket { get; set; }
         public virtual ApplicationUser Author { get; set; }
+
+
+        public static TicketAttachment CreateNewAttachmentEntry(HttpPostedFileBase postedFile, Ticket parent, IPrincipal User, ApplicationDbContext context, HttpServerUtilityBase Server) {
+            if( postedFile.ContentLength > 31457280 ) { //30MB in binary bytes
+                throw new Exception("File is too big!");
+            } else {
+
+                //get file info
+                var FileName = Path.GetFileNameWithoutExtension(postedFile.FileName);
+                var FileExt = Path.GetExtension(postedFile.FileName);
+                var FileNameModded = $"{Util.URLFriendly(FileName)}-{DateTime.Now.Ticks}{FileExt}";
+
+                //save to attachment path
+                postedFile.SaveAs(Path.Combine(Server.MapPath("~/AttachmentUploads/"), FileNameModded));
+
+                //generate model for uploaded file
+                var output = new TicketAttachment() {
+                    ParentTicketId = parent.Id
+                    , AuthorId = User.Identity.GetUserId()
+                    , MediaUrl = "/AttachmentUploads/" + FileNameModded
+                };
+
+                //return model
+                return output;
+
+            }
+
+        }
     }
 }
