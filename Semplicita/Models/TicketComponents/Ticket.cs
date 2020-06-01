a8ec1e48f30b4dedeae69b07ea08175c37dd521c
Matthew <60083197+ooobii@users.noreply.github.com>
2020-06-01 04:08:47 -0400

    Add helper methods to Ticket model
    Add assisting functions to Ticket model to make history tracking generation much easier as changes are made by users.

--- Semplicita/Models/TicketComponents/Ticket.cs
+++ Semplicita/Models/TicketComponents/Ticket.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
 
 namespace Semplicita.Models
 {
@@ -32,15 +35,196 @@ namespace Semplicita.Models
         public virtual ICollection<TicketNotification> Notifications { get; set; }
 
 
-        public string GetTicketIdentifier() {
-            return ParentProject.TicketTag + this.Id;
-        }
-
         public Ticket() {
             Attachments = new HashSet<TicketAttachment>();
             Comments = new HashSet<TicketComment>();
             HistoryEntries = new HashSet<TicketHistoryEntry>();
             Notifications = new HashSet<TicketNotification>();
         }
+
+
+        public string GetTicketIdentifier() {
+            return ParentProject.TicketTag + this.Id;
+        }
+        public TicketHistoryEntry GetCreatedHistoryEntry(IPrincipal User, ApplicationDbContext context) {
+            var history = context.TicketHistoryEntries.FirstOrDefault(the => the.EntryType == TicketHistoryEntry.TicketHistoryEntryType.Created
+                                                                          && the.ParentTicketId == this.Id);
+            if( history == null ) {
+                //does not exist in db for this ticket yet, create a new one to be added.
+                return new TicketHistoryEntry() {
+                    UserId = User.Identity.GetUserId(),
+                    ParentTicketId = this.Id,
+                    EntryType = TicketHistoryEntry.TicketHistoryEntryType.Created,
+                    OldData = null,
+                    NewData = this.Title
+                };
+
+            } else {
+                //already exists
+                return history;
+
+            }
+        }
+
+
+        public ICollection<TicketHistoryEntry> UpdateTicket(Ticket newTicket, IPrincipal User, ApplicationDbContext context) {
+            var output = new List<TicketHistoryEntry>();
+
+            //Check field changes
+            if( this.Title != newTicket.Title ) { output.Add(UpdateTitle(newTicket.Title, User, context)); }
+            if( this.Description != newTicket.Description ) { output.Add(UpdateDescription(newTicket.Description, User, context)); }
+            if( this.TicketPriorityLevelId != newTicket.TicketPriorityLevelId ) { output.Add(UpdatePriority(newTicket.TicketPriorityLevelId, User, context)); }
+            if( this.TicketTypeId != newTicket.TicketTypeId ) { output.Add(UpdateTicketType(newTicket.TicketTypeId, User, context)); }
+
+            //Check ticket assignments
+            if( this.AssignedSolverId == null & newTicket.AssignedSolverId != null ) { output.Add(UpdateSolverAssignment(newTicket.AssignedSolverId, User, context)); }
+            if( this.AssignedSolverId != null & this.AssignedSolverId != newTicket.AssignedSolverId ) { output.Add(UpdateSolverReassigned(newTicket.AssignedSolverId, User, context)); }
+            if( this.AssignedSolverId != null & newTicket.AssignedSolverId == null ) { output.Add(UpdateUnassigned(User, context)); }
+
+            //Check status
+            if( this.TicketStatusId != newTicket.TicketStatusId ) { output.Add(UpdateStatus(newTicket.TicketStatusId, User, context)); }
+
+
+            return output;
+        }
+        private TicketHistoryEntry UpdateTitle(string newValue, IPrincipal User, ApplicationDbContext context) {
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.TitleModified,
+                OldData = this.Title,
+                NewData = newValue
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.Title = newValue;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdateDescription(string newValue, IPrincipal User, ApplicationDbContext context) {
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.DescriptionModified,
+                OldData = this.Description,
+                NewData = newValue
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.Description = newValue;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdatePriority(int newId, IPrincipal User, ApplicationDbContext context) {
+            var newPriority = context.TicketPriorityTypes.Find(newId);
+
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.PriorityChanged,
+                OldData = this.TicketPriorityLevel.Name + "(" + this.TicketPriorityLevel.Rank + ")",
+                NewData = newPriority.Name + "(" + newPriority.Rank + ")"
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.TicketPriorityLevelId = newPriority.Id;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdateTicketType(int newId, IPrincipal User, ApplicationDbContext context) {
+            var newType = context.TicketTypes.Find(newId);
+
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.TicketTypeModified,
+                OldData = this.TicketType.Name + "(" + this.TicketType.Description + ")",
+                NewData = newType.Name + "(" + newType.Description + ")"
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.TicketTypeId = newType.Id;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdateSolverAssignment(string newId, IPrincipal User, ApplicationDbContext context) {
+            var newSolver = context.Users.Find(newId);
+
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.AssignedToSolver,
+                OldData = null,
+                NewData = newSolver.Id
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.AssignedSolverId = newSolver.Id;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdateSolverReassigned(string newId, IPrincipal User, ApplicationDbContext context) {
+            var newSolver = context.Users.Find(newId);
+
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.AssignedToNewSolver,
+                OldData = this.AssignedSolverId,
+                NewData = newSolver.Id
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.AssignedSolverId = newSolver.Id;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdateUnassigned(IPrincipal User, ApplicationDbContext context) {
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.UnassignedFromSolver,
+                OldData = this.AssignedSolverId,
+                NewData = null
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.AssignedSolverId = null;
+            context.SaveChanges();
+
+            return history;
+        }
+        private TicketHistoryEntry UpdateStatus(int newId, IPrincipal User, ApplicationDbContext context) {
+            var newStatus = context.TicketStatuses.Find(newId);
+
+            var history = new TicketHistoryEntry() {
+                UserId = User.Identity.GetUserId(),
+                ParentTicketId = this.Id,
+                EntryType = TicketHistoryEntry.TicketHistoryEntryType.StatusChanged,
+                OldData = this.TicketStatus.Display,
+                NewData = newStatus.Display
+            };
+
+            var ticket = context.Tickets.Find(this.Id);
+            ticket.LastInteractionAt = DateTime.Now;
+            ticket.TicketStatusId = newStatus.Id;
+            context.SaveChanges();
+
+            return history;
+        }
+
     }
 }
