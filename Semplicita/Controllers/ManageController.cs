2da69e7427bef43d175cb1180caf0e99be077aa1
Matthew <60083197+ooobii@users.noreply.github.com>
2020-07-24 21:47:05 -0400

    Added toast notifications to some manage actions
    manage > changename and manage > changepassword

--- Semplicita/Controllers/ManageController.cs
+++ Semplicita/Controllers/ManageController.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Semplicita.Models;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -55,6 +55,14 @@ namespace Semplicita.Controllers
                 : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
                 : "";
 
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
+            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
+            ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
             var userId = User.Identity.GetUserId();
             var model = new IndexViewModel {
                 HasPassword = HasPassword(),
@@ -62,10 +70,14 @@ namespace Semplicita.Controllers
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
                 BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
-                ThisUser = db.Users.Find(User.Identity.GetUserId())
+                ThisUser = db.Users.Find(User.Identity.GetUserId()),
+                CurrentLogins = userLogins,
+                OtherLogins = otherLogins
             };
             return View(model);
         }
+
+
 
         //
         // POST: /Manage/RemoveLogin
@@ -83,14 +95,9 @@ namespace Semplicita.Controllers
             } else {
                 message = ManageMessageId.Error;
             }
-            return RedirectToAction("ManageLogins", new { Message = message });
+            return RedirectToAction("Index", new { Message = message });
         }
-
-        //
-        // GET: /Manage/AddPhoneNumber
-        public ActionResult AddPhoneNumber() {
-            return View();
-        }
+        
 
         //
         // POST: /Manage/AddPhoneNumber
@@ -109,35 +116,25 @@ namespace Semplicita.Controllers
                 };
                 await UserManager.SmsService.SendAsync(message);
             }
-            return RedirectToAction("VerifyPhoneNumber", new { PhoneNumber = model.Number });
+            return RedirectToAction("Index");
         }
 
         //
-        // POST: /Manage/EnableTwoFactorAuthentication
+        // POST: /Manage/RemovePhoneNumber
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> EnableTwoFactorAuthentication() {
-            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId(), true);
+        public async Task<ActionResult> RemovePhoneNumber() {
+            var result = await UserManager.SetPhoneNumberAsync(User.Identity.GetUserId(), null);
+            if( !result.Succeeded ) {
+                return RedirectToAction("Index", new { Message = ManageMessageId.Error });
+            }
             var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
             if( user != null ) {
                 await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
             }
-            return RedirectToAction("Index", "Manage");
+            return RedirectToAction("Index", new { Message = ManageMessageId.RemovePhoneSuccess });
         }
-
-        //
-        // POST: /Manage/DisableTwoFactorAuthentication
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DisableTwoFactorAuthentication() {
-            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId(), false);
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-            if( user != null ) {
-                await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-            }
-            return RedirectToAction("Index", "Manage");
-        }
-
+        
         //
         // GET: /Manage/VerifyPhoneNumber
         public async Task<ActionResult> VerifyPhoneNumber(string phoneNumber) {
@@ -167,27 +164,38 @@ namespace Semplicita.Controllers
             return View(model);
         }
 
+
+
+
         //
-        // POST: /Manage/RemovePhoneNumber
+        // POST: /Manage/EnableTwoFactorAuthentication
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> RemovePhoneNumber() {
-            var result = await UserManager.SetPhoneNumberAsync(User.Identity.GetUserId(), null);
-            if( !result.Succeeded ) {
-                return RedirectToAction("Index", new { Message = ManageMessageId.Error });
-            }
+        public async Task<ActionResult> EnableTwoFactorAuthentication() {
+            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId(), true);
             var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
             if( user != null ) {
                 await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
             }
-            return RedirectToAction("Index", new { Message = ManageMessageId.RemovePhoneSuccess });
+            return RedirectToAction("Index");
         }
 
         //
-        // GET: /Manage/ChangePassword
-        public ActionResult ChangePassword() {
-            return View();
+        // POST: /Manage/DisableTwoFactorAuthentication
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DisableTwoFactorAuthentication() {
+            await UserManager.SetTwoFactorEnabledAsync(User.Identity.GetUserId(), false);
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if( user != null ) {
+                await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+            }
+            return RedirectToAction("Index", "Manage");
         }
+
+
+
+
 
         //
         // POST: /Manage/ChangePassword
@@ -195,7 +203,8 @@ namespace Semplicita.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model) {
             if( !ModelState.IsValid ) {
-                return View(model);
+                TempData.AddDangerToast("Unable to update your password. Please verify that you entered your current password correctly, and that both of the new passwords entered are the same.");
+                return RedirectToAction("Index");
             }
             var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
             if( result.Succeeded ) {
@@ -203,10 +212,11 @@ namespace Semplicita.Controllers
                 if( user != null ) {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                 }
+                TempData.AddSuccessToast("Your password has been updated!");
                 return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
             }
             AddErrors(result);
-            return View(model);
+            return RedirectToAction("Index");
         }
 
         //
@@ -227,35 +237,15 @@ namespace Semplicita.Controllers
                     if( user != null ) {
                         await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                     }
-                    return RedirectToAction("Index", new { Message = ManageMessageId.SetPasswordSuccess });
+                    return RedirectToAction("Index");
                 }
                 AddErrors(result);
             }
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            // If we got this far, something failed, redisplay manage page (errors will be added to alert queues)
+            return RedirectToAction("Index");
         }
-
-        //
-        // GET: /Manage/ManageLogins
-        public async Task<ActionResult> ManageLogins(ManageMessageId? message) {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-                : message == ManageMessageId.Error ? "An error has occurred."
-                : "";
-            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
-            if( user == null ) {
-                return View("Error");
-            }
-            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
-            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
-            ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
-            return View(new ManageLoginsViewModel {
-                CurrentLogins = userLogins,
-                OtherLogins = otherLogins
-            });
-        }
-
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         [MultipleButton(Name = "accountUpdate", Argument = "ChangeName")]
@@ -263,15 +253,16 @@ namespace Semplicita.Controllers
             var userId = User.Identity.GetUserId();
 
             try {
+                if( !ModelState.IsValid ) { throw new Exception(); }
                 var oldFullName = db.Users.Find(userId).FullNameStandard;
 
                 db.Users.Find(User.Identity.GetUserId()).FirstName = model.FirstName;
                 db.Users.Find(User.Identity.GetUserId()).LastName = model.LastName;
                 await db.SaveChangesAsync();
 
-                ViewBag.AccountChangeNameMessage = $"Your name has been changed from '{oldFullName}' to '{model.FirstName} {model.LastName}'.";
+                TempData.AddSuccessToast($"Your name has been changed from '{oldFullName}' to '{model.FirstName} {model.LastName}'.");
             } catch {
-                ViewBag.AccountChangeNameError = "We were unable to update your name. Please try again later.";
+                TempData.AddDangerToast("We were unable to update your name. Please try again later.");
             }
 
             var viewModel = new IndexViewModel() {
@@ -312,16 +303,25 @@ namespace Semplicita.Controllers
                 ViewBag.AccountUpdateEmailError = "The emails you entered were not the same. Please verify your email entries and try again.";
             }
 
-
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var userLogins = await UserManager.GetLoginsAsync(User.Identity.GetUserId());
+            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
+            ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
             var viewModel = new IndexViewModel() {
                 HasPassword = HasPassword(),
                 PhoneNumber = await UserManager.GetPhoneNumberAsync(userId),
                 TwoFactor = await UserManager.GetTwoFactorEnabledAsync(userId),
                 Logins = await UserManager.GetLoginsAsync(userId),
                 BrowserRemembered = await AuthenticationManager.TwoFactorBrowserRememberedAsync(userId),
-                ThisUser = db.Users.Find(User.Identity.GetUserId())
+                ThisUser = db.Users.Find(User.Identity.GetUserId()),
+                CurrentLogins = userLogins,
+                OtherLogins = otherLogins
             };
-            return View(viewModel);
+            return View("Index", viewModel);
         }
 
 
@@ -339,10 +339,10 @@ namespace Semplicita.Controllers
         public async Task<ActionResult> LinkLoginCallback() {
             var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
             if( loginInfo == null ) {
-                return RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
+                return RedirectToAction("Index", new { Message = ManageMessageId.Error });
             }
             var result = await UserManager.AddLoginAsync(User.Identity.GetUserId(), loginInfo.Login);
-            return result.Succeeded ? RedirectToAction("ManageLogins") : RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
+            return result.Succeeded ? RedirectToAction("Index") : RedirectToAction("Index", new { Message = ManageMessageId.Error });
         }
 
         protected override void Dispose(bool disposing) {
@@ -367,7 +367,8 @@ namespace Semplicita.Controllers
 
         private void AddErrors(IdentityResult result) {
             foreach( var error in result.Errors ) {
-                ModelState.AddModelError("", error);
+                TempData.Add("tdngr-iderr" + result.Errors.ToList().IndexOf(error).ToString(), error);
+                //ModelState.AddModelError("", error);
             }
         }
 
